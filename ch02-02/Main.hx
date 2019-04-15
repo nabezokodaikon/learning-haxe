11afba11dfe27d8a836e5b95aca49062420d1d17
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-04-15 10:01:38 +0900

    Add ch2.2

---
+++ ch02-02/Main.hx
@@ -0,0 +1,10 @@
+class Main {
+  static public function main(): Void {
+    var a: Int = null;
+    var b: Null<Int> = null;
+    if (a == null) {
+      trace("a = null");
+      trace(a);
+    }
+  }
+}
