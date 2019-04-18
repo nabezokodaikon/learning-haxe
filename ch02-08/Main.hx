15ccd44b99b1081dac5e51813d45416508bbfaec
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-04-18 09:04:27 +0900

    Add Up to 2.8.3

--- ch02-08/Main.hx
+++ ch02-08/Main.hx
@@ -40,6 +40,73 @@ abstract C(Int) {
   public function new() this = 3;
 }
 
+abstract MyAbstract3(String) {
+  public inline function new(s: String) {
+    this = s;
+  }
+
+  @:op(A * B)
+  public function repeat(rhs: Int): MyAbstract3 {
+    var s: StringBuf = new StringBuf();
+    for (i in 0...rhs) {
+      s.add(this);
+    }
+    return new MyAbstract3(s.toString());
+  }
+}
+
+abstract MyAbstract4(String) {
+  public inline function new(s: String) {
+    this = s;
+  }
+
+  @:op(++A)
+  public function pre() {
+    return "pre" + this;
+  }
+
+  @:op(A++)
+  public function post() {
+    return this + "post";
+  }
+}
+
+abstract MyAbstractInt(Int) from Int to Int {
+  @:op(A > B)
+  static function gt(a: MyAbstractInt, b: MyAbstractInt): Bool;
+
+  @:op(A < B)
+  static function lt(a: MyAbstractInt, b: MyAbstractInt): Bool;
+}
+
+// abstract MyArray<K, V>(Int) {
+  // @:arrayAccess
+  // public inline function get(key: K) {
+    // return this.get(key);
+  // }
+  // @:arrayAccess
+  // public inline function arrayWrite(k: K, v: V): V {
+    // this.set(k, v);
+    // return v;
+  // }
+// }
+
+abstract AString(String) {
+  public function new(s) {
+    this = s;
+  }
+
+  @:arrayAccess
+  function getInt1(k: Int) {
+    return this.charAt(k);
+  }
+
+  @:arrayAccess
+  function getInt2(k: Int) {
+    return this.charAt(k).toUpperCase();
+  }
+}
+
 class Main {
   static public function main() {
     {
@@ -68,6 +135,39 @@ class Main {
       var a: MyAbstract2 = "3";
       var b: Array<Int> = a;
       trace(b);
+    }
+
+    {
+      var a = new MyAbstract3("foo");
+      trace(a * 3);
+    }
+
+    {
+      var a = new MyAbstract4("foo");
+      trace(++a);
+      trace(a++);
+    }
+
+    {
+      var a: MyAbstractInt = 42;
+      if (a > 0) {
+        trace("Works fine, > operation implemented!");
+      }
+
+      if (a < 100) {
+        trace("" + a + "");
+      }
+    }
+
+    {
+      var map = new Map();
+      map["foo"] = 100;
+      trace(map["foo"]);
+    }
+
+    {
+      var a = new AString("foo");
+      trace(a[0]);
     }
   }
 }
