fcf9aec1de7de533a944d7aab60d4af36cbe7023
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-04-18 20:42:29 +0900

    Add ch3.7.1

---
+++ ch03-07-01/Main.hx
@@ -0,0 +1,10 @@
+import a.A;
+
+class Main {
+  static function main() {
+    var subtype1 = new a.A.B();
+    
+    var subtype2 = new B();
+    var subtype3 = new a.B();
+  }
+}
---
+++ ch03-07-01/a/A.hx
@@ -0,0 +1,13 @@
+package a;
+
+class A {
+  public function new() {
+    trace("new A");
+  }
+}
+
+class B {
+  public function new() {
+    trace("new B");
+  }
+}
