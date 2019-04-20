2c3d0ef6fd18dc5096693e3243288ef35e734c80
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-04-20 23:07:43 +0900

    Add AddDate code

---
+++ AddDate/Main.hx
@@ -0,0 +1,37 @@
+abstract AbsDate(Date) from Date to Date {
+  @:op(A + B)
+  public inline function add(step: Int): AbsDate {
+    return Date.fromTime(this.getTime() + (86400000 * step));
+  }
+
+  public inline function value(): Date {
+    return this;
+  }
+}
+
+@:forward
+abstract AbsDate2(Date) from Date to Date {
+  @:op(A + B)
+  public inline function add(step: Int): AbsDate2 {
+    return Date.fromTime(this.getTime() + (86400000 * step));
+  }
+}
+
+class Main {
+  static function main() {
+
+    trace(Date.fromTime(new Date(2019, 4, 20, 0, 0, 0).getTime()));
+
+    var date: AbsDate = new Date(2019, 4, 20, 0, 0, 0);
+    trace(date);
+    trace(date + 1);
+    trace(date + 2);
+    trace(date + 3);
+
+    var date2: AbsDate2 = new Date(2019, 4, 20, 0, 0, 0);
+    trace(date2);
+    trace(date2 + 1);
+    trace(date2 + 2);
+    trace(date2 + 3);
+  }
+}
