8882cbcb36fd0366a814934b4ae7d07d28f24b66
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-04-20 23:12:11 +0900

    Modify AddDate code

--- AddDate/Main.hx
+++ AddDate/Main.hx
@@ -4,7 +4,7 @@ abstract AbsDate(Date) from Date to Date {
     return Date.fromTime(this.getTime() + (86400000 * step));
   }
 
-  public inline function value(): Date {
+  public inline function concretization(): Date {
     return this;
   }
 }
@@ -22,16 +22,21 @@ class Main {
 
     trace(Date.fromTime(new Date(2019, 4, 20, 0, 0, 0).getTime()));
 
+
     var date: AbsDate = new Date(2019, 4, 20, 0, 0, 0);
     trace(date);
     trace(date + 1);
     trace(date + 2);
     trace(date + 3);
 
+    trace((date + 1).concretization().getTime());
+
     var date2: AbsDate2 = new Date(2019, 4, 20, 0, 0, 0);
     trace(date2);
     trace(date2 + 1);
     trace(date2 + 2);
     trace(date2 + 3);
+
+    trace((date2 + 1).getTime());
   }
 }
