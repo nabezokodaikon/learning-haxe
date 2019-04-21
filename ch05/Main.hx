0a5af0b1b46ecfbeb7d833641b2a0ae3659c2205
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-04-21 11:24:48 +0900

    Add ch5.23

--- ch05/Main.hx
+++ ch05/Main.hx
@@ -13,5 +13,24 @@ class Main {
       }
       trace(local(3));
     }
+
+    {
+      var a = cast "1";
+      trace(a);
+
+      var c = new Child(4);
+      var b = cast(c, Base);
+      trace(b);
+    }
   }
+}
+
+class Base {
+  public var value = 3;
+  public function new(v) {
+    value = v;
+  }
+}
+
+class Child extends Base {
 }
