b54fea32a03d47ece114f09569822320c3f40aac
nabezokodaikon <nabezoko.daikon@gmail.com>
2019-04-21 11:17:34 +0900

    Add ch5.11

--- ch05/Main.hx
+++ ch05/Main.hx
@@ -1,7 +1,17 @@
 class Main {
   static public function main() {
-    var a,b = "abc";
-    a = "def";
-    trace('$a, $b');
+    {
+      var a,b = "abc";
+      a = "def";
+      trace('$a, $b');
+    }
+
+    {
+      var value = 1;
+      function local(i) {
+        return value + i;
+      }
+      trace(local(3));
+    }
   }
 }
